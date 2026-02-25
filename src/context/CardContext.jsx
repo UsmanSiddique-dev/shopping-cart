e8a1e777ec66ecb4a8c7ff80c4de6ca3f0a4f6ee
Prime Labs <mrcody497@gmail.com>
2026-02-25 17:44:30 +0500

    feat:implement add to cart in UI

--- src/context/CardContext.jsx
+++ src/context/CardContext.jsx
@@ -1,26 +1,28 @@
-import { createContext,useContext,useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
-export const CartProvider=({children})=>{
-    const [cartItems,setCartItems]=useState([]);
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
 
-   function addToCart(product){
-    setCartItems((prev)=>{
-        const existingItem=prev.find((item)=>item.id===product.id);
-        if(existingItem){
-            return prev.map((item)=>
-                item.id===product.id?{...item,quantity:item.quantity+1}:item
-            );
-        }else{
-            return [...prev,{...product,quantity:1}];
-        }
+  function addToCart(product) {
+    setCartItems((prev) => {
+      const existingItem = prev.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prev.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        );
+      } else {
+        return [...prev, { ...product, quantity: 1 }];
+      }
     });
-   }
-    return(
-        <CartContext.Provider value={{cartItems,setCartItems}}>
-            {children}
-        </CartContext.Provider>
-    )
-}
-
+  }
+  return (
+    <CartContext.Provider value={{ cartItems, setCartItems,addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+export const useCart = () => useContext(CartContext);
