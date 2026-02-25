ea6c5806f66faf1d836da1238cea6563aff0ce1b
Prime Labs <mrcody497@gmail.com>
2026-02-25 17:39:05 +0500

    feat:create add to cart function

---
+++ src/context/CardContext.jsx
@@ -0,0 +1,26 @@
+import { createContext,useContext,useState } from "react";
+
+const CartContext = createContext();
+
+export const CartProvider=({children})=>{
+    const [cartItems,setCartItems]=useState([]);
+
+   function addToCart(product){
+    setCartItems((prev)=>{
+        const existingItem=prev.find((item)=>item.id===product.id);
+        if(existingItem){
+            return prev.map((item)=>
+                item.id===product.id?{...item,quantity:item.quantity+1}:item
+            );
+        }else{
+            return [...prev,{...product,quantity:1}];
+        }
+    });
+   }
+    return(
+        <CartContext.Provider value={{cartItems,setCartItems}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
